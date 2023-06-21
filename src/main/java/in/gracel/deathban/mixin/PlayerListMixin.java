a08348ebd8651dd65f9962b98745ad4a400ca109
Grace Lin <grace02lin@gmail.com>
2023-06-21 00:59:38 -0700

    updated to 1.19.3

--- src/main/java/in/gracel/deathban/mixin/PlayerListMixin.java
+++ src/main/java/in/gracel/deathban/mixin/PlayerListMixin.java
@@ -6,6 +6,7 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.server.players.PlayerList;
 import net.minecraft.server.players.UserBanList;
 import net.minecraft.server.players.UserBanListEntry;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -21,25 +22,25 @@ import java.util.Date;
 @Mixin(PlayerList.class)
 public class PlayerListMixin {
 
+    @Final
     @Shadow
     public static File USERBANLIST_FILE = new File("banned-players.json");
 
+    @Final
     @Shadow
     private UserBanList bans = new UserBanList(USERBANLIST_FILE);
 
     @Inject(at = @At("HEAD"), method = "Lnet/minecraft/server/players/PlayerList;canPlayerLogin(Ljava/net/SocketAddress;Lcom/mojang/authlib/GameProfile;)Lnet/minecraft/network/chat/Component;", cancellable = true)
-    private void canPlayerLogin(SocketAddress address, GameProfile profile, CallbackInfoReturnable<Component> callback) {
-        if (this.bans.isBanned(profile)) {
-            UserBanListEntry userbanlistentry = this.bans.get(profile);
+    private void canPlayerLogin(SocketAddress pSocketAddress, GameProfile pGameProfile, CallbackInfoReturnable<Component> cir) {
+        if (this.bans.isBanned(pGameProfile)) {
+            UserBanListEntry userbanlistentry = this.bans.get(pGameProfile);
             if (userbanlistentry.getExpires() != null) {
                 LocalDateTime ldtCurr = LocalDateTime.ofInstant(new Date().toInstant(), ZoneId.systemDefault());
-                LocalDateTime ldtExpire = LocalDateTime.ofInstant(bans.get(profile).getExpires().toInstant(), ZoneId.systemDefault());
+                LocalDateTime ldtExpire = LocalDateTime.ofInstant(bans.get(pGameProfile).getExpires().toInstant(), ZoneId.systemDefault());
                 Component component = MessageParser.banMessage(userbanlistentry.getReason(),
                         MessageParser.getTimeRemaining(ldtCurr, ldtExpire));
-                callback.setReturnValue(component);
+                cir.setReturnValue(component);
             }
         }
     }
-
-
 }
